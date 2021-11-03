223a2152471ffadfb50033751911d278d3c906a3
nitiwat sompawong <nitiwat.sompawong@aigencorp.com>
2021-11-03 22:08:29 +0700

    add predict fn

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import numpy as np
 from fastapi import FastAPI
 
 from monitoring import instrumentator
@@ -11,3 +12,21 @@ instrumentator.instrument(app).expose(app)
 async def root():
     return "helloworld"
 
+
+@app.get("/predict")
+async def predict():
+    """
+    mock classifier predict animal 
+    """
+    class_names = ["dog", "cat", "lion", "zebra"]
+    pred_prob = np.random.rand(len(class_names)).tolist()
+    pred_class_prob = np.max(pred_prob).item()
+    pred_class = class_names[np.argmax(pred_prob)]
+    
+    prediction = {
+        "pred_class": pred_class,
+        "pred_class_prob": pred_class_prob,
+        "pred_prob": pred_prob,
+    }
+
+    return prediction
