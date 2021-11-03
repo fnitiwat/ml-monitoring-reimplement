fd9af2b26b0d087f26c22c174004eaeb19e78a62
nitiwat sompawong <nitiwat.sompawong@aigencorp.com>
2021-11-03 23:01:36 +0700

    add monitor:  pred_prob_model_output

--- app/monitoring.py
+++ app/monitoring.py
@@ -1,5 +1,10 @@
 import os
+import random
+import numpy as np
+from typing import Callable
+from prometheus_client import Histogram
 from prometheus_fastapi_instrumentator import Instrumentator, metrics
+from prometheus_fastapi_instrumentator.metrics import Info
 
 
 NAMESPACE = os.environ.get("METRICS_NAMESPACE", "fastapi")
@@ -17,6 +22,36 @@ instrumentator = Instrumentator(
     # inprogress_labels=True,
 )
 
+# TODO:
+# custom metric
+# avg class per sec (couter)
+# process time (instant vector)
+# pred prob lower than threshold (alert) (histogram)
+def pred_prob_model_output(
+    metric_name: str = "pred_prob_model_output",
+    metric_doc: str = "pred_prob_model_output",
+    metric_namespace: str = "",
+    metric_subsystem: str = "",
+    buckets=(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, float("inf")),
+) -> Callable[[Info], None]:
+    METRIC = Histogram(
+        metric_name,
+        metric_doc,
+        buckets=buckets,
+    )
+    def instrumentation(info: Info) -> None:
+        if info.modified_handler == "/predict":
+            # pred_prob = info.response.body
+            pred_prob = random.uniform(0, 1)
+            if pred_prob:
+                METRIC.observe(float(pred_prob))
+
+    return instrumentation
+
+
+instrumentator.add(
+    pred_prob_model_output(metric_namespace=NAMESPACE, metric_subsystem=SUBSYSTEM)
+)
 
 instrumentator.add(
     metrics.request_size(
